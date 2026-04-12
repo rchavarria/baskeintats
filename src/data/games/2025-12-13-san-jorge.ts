b4652ecf63d5a56b9493aad4ec212456b005f5c9
Rubén Chavarría <rchavarria@easyvista.com>
2026-04-12 08:41:24 +0200

    ✨ New match: Ahorramás League, Silver, Phase 1, J10, San Jorge

---
+++ src/data/games/2025-12-13-san-jorge.ts
@@ -0,0 +1,83 @@
+import { GameSchema } from "../../model/GameSchema.ts";
+import type { Game } from "../../model/GameSchema.ts";
+import { venues } from "../venues.ts";
+import { teams } from "../teams.ts";
+
+export const game_2025_12_13_san_jorge: Game = GameSchema.parse({
+  id: "S56-liga-u15-j10",
+  season: "2025-26",
+  date: "2025-12-13T19:15:00Z",
+  type: "game",
+
+  competition: {
+    name: "Liga Ahorramás - Plata",
+    category: "U15M",
+    phase: "",
+    round: "J10",
+  },
+
+  venue: venues["diaz-miguel"],
+
+  home: {
+    club: teams["san-jorge"],
+    category: "U15M",
+    scores: [19, 25, 18, 20],
+  },
+
+  away: {
+    club: teams["alcobendas"],
+    category: "U15M",
+    scores: [25, 18, 21, 13],
+  },
+
+  playerStats: {
+    time: 17 * 60 + 44,
+    fieldGoals: 2,
+    threePointers: 0,
+    freeThrows: {
+      made: 0,
+      attempted: 0,
+    },
+    faults: 4,
+    plusMinus: 10,
+    efficiency: 0,
+  },
+
+  videos: {
+    official: "https://youtu.be/-jlGneJLtks",
+    others: [],
+  },
+
+  recap: {
+    title: "¡Partido muy competido!",
+    lines: [
+      "Qué lástima, casi lo tuvimos, no pudimos aguantar la ventaja que tomamos al principio del partido",
+      "Las faltas nos lastraron y no pudimos jugar todo lo que quisimos",
+      "Aun así, buen partido, competido hasta el final, que seguro que los nuestros sabrán aprovechar para crecer como jugadores",
+    ],
+  },
+
+  references: [
+    {
+      icon: "📸",
+      label: "Fotos del mes de diciembre",
+      url: "https://www.flickr.com/photos/fbmadrid/albums/72177720330932743",
+    },
+    {
+      icon: "💼",
+      label: "Informe de la jornada",
+      url: "https://fbm.es/informes.aspx?delegacion=1&grupo=14618&informe=resultados-clasificacion-proxima&ultima_jornada=93841&proxima_jornada=93842",
+    },
+    {
+      icon: "📰",
+      label: "Previa del partido",
+      url: "https://fbm.es/noticia-123-12977/liga-ahorramas-suspense-hasta-el-final",
+    },
+    {
+      icon: "📰",
+      label: "Crónica del partido",
+      url: "https://www.fbm.es/noticia-104-12989/concluye-la-primera-fase-de-la-liga-ahorramas",
+    },
+  ],
+});
+
