e8185f1f89a2ee2ea6ace28ff6439cde7bf2327a
Rubén Chavarría <rchavarria@easyvista.com>
2026-04-13 16:16:31 +0200

    ✨ New match: Ahorramás League, Gold, Phase 1, J9, Patrocinio

---
+++ src/data/games/2025-12-13-patrocinio.ts
@@ -0,0 +1,80 @@
+import {GameSchema} from "../../model/GameSchema.ts";
+import type {Game} from "../../model/GameSchema.ts";
+import {venues} from "../venues.ts";
+import {teams} from "../teams.ts";
+
+export const game_2025_12_13_patrocinio: Game = GameSchema.parse({
+  id: "S56-liga-u16-j8",
+  type: "game",
+  season: "2025-26",
+  date: "2025-12-13T12:15:00Z",
+
+  competition: {
+    name: "Liga Ahorramás - Oro",
+    category: "U16M",
+    phase: "",
+    round: "J8",
+  },
+
+  venue: venues["antela"],
+
+  home: {
+    club: teams["alcobendas"],
+    category: "U15M",
+    scores: [22, 18, 15, 18],
+  },
+
+  away: {
+    club: teams["patrocinio"],
+    category: "U15M",
+    scores: [27, 18, 20, 15],
+  },
+
+  playerStats: {
+    time: 0,
+    fieldGoals: 0,
+    threePointers: 0,
+    freeThrows: {
+      made: 0,
+      attempted: 0,
+    },
+    faults: 0,
+    plusMinus: 0,
+    efficiency: 0,
+  },
+
+  videos: {
+    official: "https://youtu.be/C_to-RQhfK4",
+    others: [],
+  },
+
+  recap: {
+    title: "",
+    lines: [],
+  },
+
+  references: [
+    {
+      icon: "📸",
+      label: "Fotos del mes de diciembre",
+      url: "https://www.flickr.com/photos/fbmadrid/albums/72177720330932743",
+    },
+    {
+      icon: "💼",
+      label: "Informe de la jornada",
+      url: "https://fbm.es/informes.aspx?delegacion=1&grupo=14615&informe=resultados-clasificacion-proxima&ultima_jornada=93799&proxima_jornada=93800",
+    },
+    {
+      icon: "📰",
+      label: "Previa del partido",
+      url: "https://fbm.es/noticia-123-12977/liga-ahorramas-suspense-hasta-el-final",
+    },
+    {
+      icon: "📰",
+      label: "Crónica del partido",
+      url: "https://www.fbm.es/noticia-104-12989/concluye-la-primera-fase-de-la-liga-ahorramas",
+    },
+  ],
+
+});
+
