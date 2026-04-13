753be2044fbf6aa26e440333aca80087e901effb
Rubén Chavarría <rchavarria@easyvista.com>
2026-04-13 16:16:17 +0200

    ✨ New match: Ahorramás League, Gold, Phase 1, J7, Las Rozas

---
+++ src/data/games/2025-11-16-rozas.ts
@@ -0,0 +1,104 @@
+import {AdvancedGameSchema} from "../../model/GameSchema.ts";
+import type {AdvancedGame} from "../../model/GameSchema.ts";
+import {venues} from "../venues.ts";
+import {teams} from "../teams.ts";
+
+export const game_2025_11_16_rozas: AdvancedGame = AdvancedGameSchema.parse({
+  id: "S56-liga-u16-j7",
+  type: "advanced-game",
+  season: "2025-26",
+  date: "2025-11-16T14:30:00Z",
+
+  competition: {
+    name: "Liga Ahorramás - Oro",
+    category: "U16M",
+    phase: "",
+    round: "J7",
+  },
+
+  venue: venues["espiniella"],
+
+  home: {
+    club: teams["rozas"],
+    category: "U15M",
+    scores: [16, 10, 15, 21],
+  },
+
+  away: {
+    club: teams["alcobendas"],
+    category: "U15M",
+    scores: [14, 15, 23, 20],
+  },
+
+  playerStats: {
+    time: 18 * 60 + 7,
+    fieldGoals: {
+      made: 3,
+      attempted: 6,
+    },
+    threePointers: {
+      made: 0,
+      attempted: 0,
+    },
+    freeThrows: {
+      made: 0,
+      attempted: 0,
+    },
+    rebounds: {
+      offensive: 0,
+      defensive: 2,
+    },
+    assists: 1,
+    steals: 1,
+    turnovers: 2,
+    blocks: {
+      made: 0,
+      received: 0,
+    },
+    faults: {
+      made: 4,
+      received: 1,
+    },
+    plusMinus: 8,
+    efficiency: 2,
+  },
+
+  videos: {
+    official: "https://youtu.be/pMm3kJcVirs",
+    others: [],
+  },
+
+  recap: {
+    title: "¡Revancha tomada!",
+    lines: [
+      "Esta vez sí, esta vez jugaron bien y conseguimos vencerles",
+      "Durante la primera mitad estuvo bastante igualado, hasta que pudimos sacar una ventaja, que luego supimos mantener",
+      "Su número 5 acabó bastante desquiciado, quería hacerse el héroe y salió trasquilado",
+    ],
+  },
+
+  references: [
+    {
+      icon: "📸",
+      label: "Fotos del mes de noviembre",
+      url: "https://www.flickr.com/photos/fbmadrid/sets/72177720330208384/",
+    },
+    {
+      icon: "💼",
+      label: "Informe de la jornada",
+      url: "https://fbm.es/informes.aspx?delegacion=1&grupo=14615&informe=resultados-clasificacion-proxima&ultima_jornada=93798&proxima_jornada=93799",
+    },
+    {
+      icon: "📰",
+      label: "Previa del partido",
+      url: "https://www.fbm.es/noticia-104-12920/grandes-duelos-en-la-liga-ahorramas",
+    },
+    {
+      icon: "📰",
+      label: "Crónica del partido",
+      url: "https://www.fbm.es/noticia-104-12931/sube-la-temperatura-en-la-liga-ahorramas",
+    },
+  ],
+
+});
+
