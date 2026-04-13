07a79d708ce4a8fa03c4600737b353a21280f449
Rubén Chavarría <rchavarria@easyvista.com>
2026-04-13 16:16:23 +0200

    ✨ New match: Ahorramás League, Gold, Phase 1, J8, Estudiantes

---
+++ src/data/games/2025-11-30-estudiantes.ts
@@ -0,0 +1,106 @@
+import {AdvancedGameSchema} from "../../model/GameSchema.ts";
+import type {AdvancedGame} from "../../model/GameSchema.ts";
+import {venues} from "../venues.ts";
+import {teams} from "../teams.ts";
+
+export const game_2025_11_30_estudiantes: AdvancedGame = AdvancedGameSchema.parse({
+  id: "S56-liga-u16-j9",
+  type: "advanced-game",
+  season: "2025-26",
+  date: "2025-11-30T10:30:00Z",
+
+  competition: {
+    name: "Liga Ahorramás - Oro",
+    category: "U16M",
+    phase: "",
+    round: "J9",
+  },
+
+  venue: venues["caja-magica"],
+
+  home: {
+    club: teams["estudiantes"],
+    category: "U15M",
+    scores: [31, 32, 22, 18],
+  },
+
+  away: {
+    club: teams["alcobendas"],
+    category: "U15M",
+    scores: [15, 7, 18, 12],
+  },
+
+  playerStats: {
+    time: 20 * 60 + 27,
+    fieldGoals: {
+      made: 3,
+      attempted: 3,
+    },
+    threePointers: {
+      made: 0,
+      attempted: 0,
+    },
+    freeThrows: {
+      made: 0,
+      attempted: 0,
+    },
+    rebounds: {
+      offensive: 0,
+      defensive: 2,
+    },
+    assists: 0,
+    steals: 1,
+    turnovers: 2,
+    blocks: {
+      made: 0,
+      received: 0,
+    },
+    faults: {
+      made: 4,
+      received: 2,
+    },
+    plusMinus: -32,
+    efficiency: 5,
+  },
+
+  videos: {
+    official: "https://youtu.be/wnIlTg-M_7A",
+    others: [],
+  },
+
+  recap: {
+    title: "¡No se pudo hacer nada!",
+    lines: [
+      "Salimos un poco decepcionados, creo que lo hicimos peor de lo que esperábamos",
+      "Veníamos haciendo buenos partidos, y hoy había esperanzas de hacerlo bien",
+      "Pero trajeron a un gigante, y nos entró el canguelo",
+      "Un primer cuarto flojo, y un segundo más flojo todavía lastraron todo",
+      "Pero no bajaron los brazos del todo y siguieron compitiendo",
+    ],
+  },
+
+  references: [
+    {
+      icon: "📸",
+      label: "Fotos del mes de noviembre",
+      url: "https://www.flickr.com/photos/fbmadrid/sets/72177720330208384/",
+    },
+    {
+      icon: "💼",
+      label: "Informe de la jornada",
+      url: "https://fbm.es/informes.aspx?delegacion=1&grupo=14615&informe=resultados-clasificacion-proxima&ultima_jornada=93800&proxima_jornada=93801",
+    },
+    {
+      icon: "📰",
+      label: "Previa del partido",
+      url: "https://www.fbm.es/noticia-104-12956/liga-ahorramas-nadie-quiere-quedarse-atr%C3%A1s",
+    },
+    {
+      icon: "📰",
+      label: "Crónica del partido",
+      url: "https://www.fbm.es/noticia-123-12967/liga-ahorramas-emoci%C3%B3n-hasta-el-final",
+    },
+  ],
+
+});
+
