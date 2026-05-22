d064c9aff48d222ac8d38c64ca29c444a387301a
Rubén Chavarría <txingo@gmail.com>
2026-05-22 13:45:14 +0200

    🏗️ Bundle code differently
    Split bundles, one for React and related deps, another one for my own code

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router')) {
+            return 'vendor-react'
+          }
+        },
+      },
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
