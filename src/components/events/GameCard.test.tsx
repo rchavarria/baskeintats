ce707ac4c6d855a33f00f7e885202bea896c55a2
Rubén Chavarría <txingo@gmail.com>
2026-05-26 09:22:59 +0200

    ✨ New game: Ahorramás League, Silver, Phase 3, J4, Zentro Basket

--- src/components/events/GameCard.test.tsx
+++ src/components/events/GameCard.test.tsx
@@ -33,6 +33,7 @@ import {game_2026_05_14_fuenlabrada} from "../../data/games/2026-05-14-fuenlabra
 import {game_2026_05_24_distrito} from "../../data/games/2026-05-24-distrito.ts";
 import {game_2026_05_24_real_madrid} from "../../data/games/2026-05-24-real-madrid.ts";
 import {game_2026_05_27_distrito} from "../../data/games/2026-05-27-distrito.ts";
+import {game_2026_05_30_zentro} from "../../data/games/2026-05-30-zentro.ts";
 
 const fakeGame: Game = game_2025_09_20_alcorcon;
 
@@ -88,6 +89,7 @@ describe("GameCard", () => {
   });
 
   it.each([
+    [game_2026_05_30_zentro],
     [game_2026_05_27_distrito],
     [game_2026_05_24_real_madrid],
     [game_2026_05_24_distrito],
