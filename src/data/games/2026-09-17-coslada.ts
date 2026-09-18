4d75899b0f8154a4d33bc88db8aa8ed23583ffea
Rubén Chavarría <txingo@gmail.com>
2026-09-18 12:56:28 +0200

    🍱 Add recap

--- src/data/games/2026-09-17-coslada.ts
+++ src/data/games/2026-09-17-coslada.ts
@@ -40,8 +40,14 @@ export const game_2026_09_17_coslada: FriendlyGame = FriendlyGameSchema.parse({
   },
 
   recap: {
-    title: "Primer partido... con muchos nervios",
-    lines: ["..."],
+    title: "Pagamos caros los nervios del primer cuarto",
+    lines: [
+      "Uff, menudo arranque más malo.",
+      "Los nervios nos jugaron una mala pasada y nos costó entrar en el partido. El primer cuarto fue un desastre.",
+      "Luego tampoco es que mejoráramos mucho. Nos faltó ser más duros en defensa y cerrar más el rebote.",
+      "Pero hay que tener en cuenta que son 2 años mayores que nosotros, y que era el primer partido.",
+      "A seguir aprendiendo y trabajando, que hay que ponerse a tono para la liga.",
+    ],
   },
 
   references: [],
